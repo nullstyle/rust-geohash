341a8bc13be866fb65ed9fa11d4bd91235536e0a
Scott Fleckenstein <nullstyle@gmail.com>
2013-08-20 21:56:27 -0700

    Removes unused *_half methods

--- geohash/interval.rs
+++ geohash/interval.rs
@@ -18,9 +18,6 @@ impl Interval {
   pub fn high(&self) -> f64 {
     self.hi
   }
-
-  pub fn upper_half(&self) -> Interval { Interval{ lo:self.median(), hi:self.hi } }
-  pub fn lower_half(&self) -> Interval { Interval{ lo:self.lo, hi:self.median() } }
 }
 
 pub fn contract_interval(interval:&mut Interval, contract_hi:bool) {
